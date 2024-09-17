9fb68cf7bb5cc4e7ba43151bfad3cccc1fed6f2a
IHomoSapienI <s-2221998@hotmail.com>
2024-09-17 11:24:38 -0500

    habee

--- controllers/rol.js
+++ controllers/rol.js
@@ -6,8 +6,7 @@ const Permiso = require('../modules/permiso');
 // Método GET para obtener los roles
 const rolesGet = async (req, res = response) => {
     try {
-        const roles = await Rol.find().populate('permisoRol', 'nombrePermiso'); // Solo trae el campo nombrePermiso
-        res.json({ roles }); // Consultar todos los documentos de la colección
+        const roles = await Rol.find(); // Consultar todos los documentos de la colección
 
         if (roles.length === 0) {
             return res.status(404).json({
