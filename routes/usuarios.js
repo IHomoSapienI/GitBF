40f69dba791961ed90c1434f35eda462778321bc
IHomoSapienI <s-2221998@hotmail.com>
2024-10-08 11:59:59 -0500

    adas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,9 @@
+const { Router } = require('express');
 const express = require('express');
 const { validarJWT } = require('../middlewares/verificartoken'); // Importar el middleware
+const verificarPermisos = require('../middlewares/verificarPermisos'); // Asegúrate de que la ruta sea correcta
+
+
 const {
     usuariosGet,
     usuariosPost,
@@ -10,15 +14,15 @@ const {
 const router = express.Router();
 
 // Ruta pública para registrar un usuario
-router.post('/', usuariosPost);
+router.post('/', verificarPermisos (['crearUsuarios']), usuariosPost);
 
 // Ruta privada para obtener usuarios (requiere autenticación)
-router.get('/',  usuariosGet); 
+router.get('/',  verificarPermisos (['verUsuarios']), usuariosGet); 
 
 // Ruta privada para actualizar un usuario (requiere autenticación)
-router.put('/:id',  usuariosPut); 
+router.put('/:id',  verificarPermisos (['actualizarUsuarios']), usuariosPut); 
 
 // Ruta privada para eliminar un usuario (requiere autenticación)
-router.delete('/:id',  usuariosDelete);
+router.delete('/:id',  verificarPermisos (['eliminarUsuarios']), usuariosDelete);
 
 module.exports = router;
