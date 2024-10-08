db35c87af75dfaf452fa2230c4a6711262aec71e
IHomoSapienI <s-2221998@hotmail.com>
2024-10-07 19:50:56 -0500

    vaidnado

--- middlewares/verificarPermisos.js
+++ middlewares/verificarPermisos.js
@@ -5,18 +5,24 @@ const verificarPermisos = (permisosRequeridos) => {
     return async (req, res, next) => {
         try {
             const userId = req.uid; // uid del middleware validarJWT
-            const usuario = await Usuario.findById(userId).populate('rol');
+            console.log(`UID del usuario: ${userId}`); // Para verificar si uid está llegando correctamente
 
+            const usuario = await Usuario.findById(userId).populate('rol');
+            console.log(`Usuario encontrado: ${usuario}`); // Para verificar el usuario encontrado
+            
             if (!usuario || !usuario.rol) {
                 return res.status(403).json({ msg: 'Usuario o rol no encontrado' });
             }
 
             const permisosUsuario = usuario.rol.permisoRol; // IDs de permisos del rol
-            
+            console.log(`Permisos del usuario: ${permisosUsuario}`); // Para verificar los permisos
+
             const permisosValidos = await Permiso.find({ _id: { $in: permisosUsuario } });
             const permisosValidosNombres = permisosValidos.map(p => p.nombrePermiso);
+            console.log(`Permisos válidos: ${permisosValidosNombres}`); // Para verificar los permisos válidos
             
             const tienePermisos = permisosRequeridos.every(permiso => permisosValidosNombres.includes(permiso));
+            console.log(`¿Usuario tiene permisos? ${tienePermisos}`); // Para verificar si tiene permisos
             
             if (!tienePermisos) {
                 return res.status(403).json({ msg: 'No tienes permiso para acceder a esta ruta' });
