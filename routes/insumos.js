4e0730c6bc01daec5db2a34ad2d036c79b8c836d
IHomoSapienI <s-2221998@hotmail.com>
2024-10-08 20:04:03 -0500

    asdsa

--- routes/insumos.js
+++ routes/insumos.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const { obtenerInsumos, crearInsumo, actualizarInsumo, eliminarInsumo, cambiarEstadoInsumo } = require('../controllers/insumo');
+const { Router } = require('express');
 
+const { obtenerInsumos, crearInsumo, actualizarInsumo, eliminarInsumo, cambiarEstadoInsumo } = require('../controllers/insumo');
+const { validarJWT } = require('../middlewares/verificartoken'); // Asegúrate de que la ruta sea correcta
+const verificarPermisos = require('../middlewares/verificarPermisos'); // Asegúrate de que la ruta sea correcta
+const router = Router();
+router.use(validarJWT);
 // Obtener todos los insumos
-router.get('/', obtenerInsumos);
+router.get('/', verificarPermisos (['verInsumo']), obtenerInsumos);
 
 // Crear un nuevo insumo
-router.post('/', crearInsumo);
+router.post('/', verificarPermisos (['crearInsumo']),crearInsumo);
 
 // Actualizar un insumo por ID
-router.put('/:id', actualizarInsumo);
+router.put('/:id', verificarPermisos (['actualizarInsumo']), actualizarInsumo);
 
 // Eliminar un insumo por ID
-router.delete('/:id', eliminarInsumo);
+router.delete('/:id', verificarPermisos (['eliminarInsumo']),eliminarInsumo);
 
 // Cambiar el estado de un insumo por ID
 router.patch('/:id/estado', cambiarEstadoInsumo);
