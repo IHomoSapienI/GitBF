004ee451f65643f4feb8cda747dd62b2b302fded
IHomoSapienI <s-2221998@hotmail.com>
2024-10-16 19:23:56 -0500

    asdas

--- controllers/userHelper.js
+++ controllers/userHelper.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const User = require('../modules/usuario');
+const Rol = require('../modules/rol');
 
 const createUser = async ({ nombre, email, password, rol, estado }) => {
     try {
@@ -7,15 +8,25 @@ const createUser = async ({ nombre, email, password, rol, estado }) => {
         const hashedPassword = await bcrypt.hash(password, salt);
         console.log('Contraseña encriptada antes de guardar:', hashedPassword);
 
+        // Fetch the complete role object
+        const roleObject = await Rol.findById(rol);
+        if (!roleObject) {
+            throw new Error('Rol no encontrado');
+        }
+
         const newUser = new User({
             nombre,
             email,
             password: hashedPassword,
-            rol,
+            rol: roleObject._id,
             estado
         });
 
         await newUser.save();
+        
+        // Populate the rol field after saving
+        await newUser.populate('rol');
+
         console.log('Usuario creado exitosamente:', JSON.stringify(newUser, null, 2));
 
         return newUser;
