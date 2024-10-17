acdc9bc38ff4ff1de4815f8aaa734eee3d40101b
IHomoSapienI <s-2221998@hotmail.com>
2024-10-17 08:07:19 -0500

    adas

--- routes/clientes.js
+++ routes/clientes.js
@@ -7,20 +7,22 @@ const {
     actualizarCliente,
     eliminarCliente
 } = require('../controllers/cliente');
-
+const { validarJWT } = require('../middlewares/verificartoken'); // Asegúrate de que la ruta sea correcta
+const verificarPermisos = require('../middlewares/verificarPermisos'); 
+router.use(validarJWT);
 // Ruta para crear un nuevo cliente
-router.post('/', crearCliente);
+router.post('/', verificarPermisos(['verCliente']), crearCliente);
 
 // Ruta para obtener todos los clientes
-router.get('/', obtenerClientes);
+router.get('/', verificarPermisos(['crearCliente']),obtenerClientes);
 
 // Ruta para obtener un cliente por ID
-router.get('/:id', obtenerClientePorId);
+router.get('/:id', verificarPermisos(['verCliente']),obtenerClientePorId);
 
 // Ruta para actualizar un cliente por ID
-router.put('/:id', actualizarCliente);
+router.put('/:id', verificarPermisos(['actualizarCompras']),actualizarCliente);
 
 // Ruta para eliminar un cliente por ID
-router.delete('/:id', eliminarCliente);
+router.delete('/:id',verificarPermisos(['eliminarCompras']), eliminarCliente);
 
 module.exports = router;
