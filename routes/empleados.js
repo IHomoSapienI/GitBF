fca7b57bd4db8b5004b104c1f9418e2a77702873
IHomoSapienI <s-2221998@hotmail.com>
2024-10-21 09:17:02 -0500

    asdas

--- routes/empleados.js
+++ routes/empleados.js
@@ -1,26 +1,23 @@
-const express = require('express');
-const router = express.Router();
-const {
-    crearEmpleado,
-    obtenerEmpleados,
-    obtenerEmpleadoPorId,
-    actualizarEmpleado,
-    eliminarEmpleado
-} = require('../controllers/empleado');
+const { Router } = require('express');
+const { crearEmpleado, obtenerEmpleados, obtenerEmpleadoPorId, actualizarEmpleado, eliminarEmpleado } = require('../controllers/empleado');
+const { validarJWT } = require('../middlewares/verificartoken'); // Asegúrate de que la ruta sea correcta
+const verificarPermisos = require('../middlewares/verificarPermisos'); 
+router.use(validarJWT);
 
+const router = Router();
 // Ruta para crear un nuevo empleado
-router.post('/', crearEmpleado);
+router.post('/', verificarPermisos (['crearEmpleado']),crearEmpleado);
 
 // Ruta para obtener todos los empleados
-router.get('/', obtenerEmpleados);
+router.get('/', verificarPermisos (['verEmpleado']), obtenerEmpleados);
 
 // Ruta para obtener un empleado por ID
-router.get('/:id', obtenerEmpleadoPorId);
+router.get('/:id', verificarPermisos (['verEmpleado']), obtenerEmpleadoPorId);
 
 // Ruta para actualizar un empleado por ID
-router.put('/:id', actualizarEmpleado);
+router.put('/:id', verificarPermisos (['actualizarEmpleado']),actualizarEmpleado);
 
 // Ruta para eliminar un empleado por ID
-router.delete('/:id', eliminarEmpleado);
+router.delete('/:id', verificarPermisos (['eliminarEmpleado']),eliminarEmpleado);
 
 module.exports = router;
