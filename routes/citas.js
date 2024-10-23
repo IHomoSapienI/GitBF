f5d0d0248773208a79dd9f424151c06e2ef6b2e7
IHomoSapienI <s-2221998@hotmail.com>
2024-10-23 10:05:15 -0500

    asdas

--- routes/citas.js
+++ routes/citas.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const {
-    crearCita,
-    obtenerCitas,
-    obtenerCitaPorId,
-    actualizarCita,
-    eliminarCita
-} = require('../controllers/cita');
+const { Router } = require('express');
 
+
+const { crearCita, obtenerCitas, obtenerCitaPorId, actualizarCita, eliminarCita} = require('../controllers/cita');
+const { validarJWT } = require('../middlewares/verificartoken'); // Asegúrate de que la ruta sea correcta
+const verificarPermisos = require('../middlewares/verificarPermisos'); // Asegúrate de que la ruta sea correcta
+const router = Router();
+router.use(validarJWT);
 // Definir las rutas para citas
-router.post('/', crearCita);
-router.get('/', obtenerCitas);
-router.get('/:id', obtenerCitaPorId);
-router.put('/:id', actualizarCita);
-router.delete('/:id', eliminarCita);
+router.post('/', verificarPermisos (['crearCita']), crearCita);
+router.get('/', verificarPermisos (['verCita']),obtenerCitas);
+router.get('/:id', verificarPermisos (['verCita']), obtenerCitaPorId);
+router.put('/:id', verificarPermisos (['actualizarCita']), actualizarCita);
+router.delete('/:id', verificarPermisos (['eliminarCita']),eliminarCita);
 
 module.exports = router;
