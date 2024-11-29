16cc95ae6be26e5c50aef6459ac073e39842258f
IHomoSapienI <s-2221998@hotmail.com>
2024-11-29 08:24:29 -0500

    asdas

--- routes/permisos.js
+++ routes/permisos.js
@@ -4,7 +4,7 @@ const { validarJWT } = require('../middlewares/verificartoken'); // Asegúrate d
 const verificarPermisos = require('../middlewares/verificarPermisos'); // Asegúrate de que la ruta sea correcta
 const {permisosGet, permisosPost, permisosPut, permisosDelete} =require ('../controllers/permiso');
 const router = Router();
-router.use(validarJWT);
+// router.use(validarJWT);
 
 //router.get('/', verificarPermisos (['verPermisos']),permisosGet);
 //router.post('/', verificarPermisos (['crearPermiso']),permisosPost);
