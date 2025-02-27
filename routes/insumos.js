1c41354bcc13b619609a8eecdd0f68ebb95dc309
IHomoSapienI <s-2221998@hotmail.com>
2025-02-26 21:24:37 -0500

    Desbloqueo de Comrpas

--- routes/insumos.js
+++ routes/insumos.js
@@ -1,23 +1,40 @@
 const { Router } = require('express');
 
 const { obtenerInsumos, crearInsumo, actualizarInsumo, eliminarInsumo, cambiarEstadoInsumo } = require('../controllers/insumo');
-const { validarJWT } = require('../middlewares/verificartoken'); // Asegúrate de que la ruta sea correcta
-const verificarPermisos = require('../middlewares/verificarPermisos'); // Asegúrate de que la ruta sea correcta
+//const { validarJWT } = require('../middlewares/verificartoken'); // Asegúrate de que la ruta sea correcta
+//const verificarPermisos = require('../middlewares/verificarPermisos'); // Asegúrate de que la ruta sea correcta
 const router = Router();
-router.use(validarJWT);
+//router.use(validarJWT);
+// // Obtener todos los insumos
+// router.get('/', verificarPermisos (['verInsumo']), obtenerInsumos);
+
+// // Crear un nuevo insumo
+// router.post('/', verificarPermisos (['crearInsumo']),crearInsumo);
+
+// // Actualizar un insumo por ID
+// router.put('/:id', verificarPermisos (['actualizarInsumo']), actualizarInsumo);
+
+// // Eliminar un insumo por ID
+// router.delete('/:id', verificarPermisos (['eliminarInsumo']),eliminarInsumo);
+
+// // Cambiar el estado de un insumo por ID
+// router.patch('/:id/estado', cambiarEstadoInsumo);
+
 // Obtener todos los insumos
-router.get('/', verificarPermisos (['verInsumo']), obtenerInsumos);
+router.get('/',  obtenerInsumos);
 
 // Crear un nuevo insumo
-router.post('/', verificarPermisos (['crearInsumo']),crearInsumo);
+router.post('/', crearInsumo);
 
 // Actualizar un insumo por ID
-router.put('/:id', verificarPermisos (['actualizarInsumo']), actualizarInsumo);
+router.put('/:id',  actualizarInsumo);
 
 // Eliminar un insumo por ID
-router.delete('/:id', verificarPermisos (['eliminarInsumo']),eliminarInsumo);
+router.delete('/:id', eliminarInsumo);
 
 // Cambiar el estado de un insumo por ID
 router.patch('/:id/estado', cambiarEstadoInsumo);
+
+
 
 module.exports = router;
