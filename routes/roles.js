8ea1c3edf9045b90b535d38316ed394097369305
IHomoSapienI <s-2221998@hotmail.com>
2025-03-08 10:36:39 -0500

    CAmbios en estado del Rol

--- routes/roles.js
+++ routes/roles.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const {rolesGet, rolesPost,rolesPut, rolesDelete} =require ('../controllers/rol');
+const {rolesGet, rolesPost,rolesPut, rolesDelete, rolesToggleEstado} =require ('../controllers/rol');
 const { validarJWT } = require('../middlewares/verificartoken'); // Asegúrate de que la ruta sea correcta
 const verificarPermisos = require('../middlewares/verificarPermisos'); // Asegúrate de que la ruta sea correcta
 
@@ -14,5 +14,8 @@ router.post('/', verificarPermisos (['crearRol']),rolesPost);
 router.put('/:id', verificarPermisos (['actualizarRol']),rolesPut);
 
 router.delete('/:id', verificarPermisos (['eliminarRol']),rolesDelete);
+
+// Nueva ruta para cambiar el estado de un rol
+router.patch("/:id/toggle-estado", verificarPermisos(["actualizarRol"]), rolesToggleEstado)
 
 module.exports = router;
