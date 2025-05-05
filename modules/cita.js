790f2df961c53f29992dc9912cccc247956cbac7
IHomoSapienI <s-2221998@hotmail.com>
2025-05-05 17:46:18 -0500

    retroceso

--- modules/cita.js
+++ modules/cita.js
@@ -1,56 +1,53 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require("mongoose");
+
 
 const CitaSchema = Schema({
-  nombreempleado: {
-    type: Schema.Types.ObjectId,
-    ref: "Empleado",
-    required: true,
-  },
-  nombrecliente: {
-    type: Schema.Types.ObjectId,
-    ref: "Cliente",
-    required: true,
-  },
-  fechacita: {
-    type: Date,
-    required: true,
-  },
-  horacita: {
-    // Cambiado de String a Date
-    type: Date,
-    required: true,
-  },
-  duracionTotal: {
-    // Duración total en minutos
-    type: Number,
-    required: true,
-  },
-  motivo: {
-    type: String,
-  },
-  fechacancelacion: {
-    type: Date,
-  },
-  montototal: {
-    type: Number,
-    required: true,
-  },
-  estadocita: {
-    type: String,
-    enum: ["Pendiente", "Confirmada", "En Progreso", "Completada", "Cancelada"],
-    default: "Pendiente",
-  },
-  servicios: [
-    {
-      servicio: {
+    nombreempleado: {
         type: Schema.Types.ObjectId,
-        ref: "Servicio",
-      },
-      nombreServicio: String,
-      precio: Number,
-      tiempo: Number,
+        ref: 'Empleado',
+        required: true
     },
-  ],
-})
+    nombrecliente: {
+        type: Schema.Types.ObjectId,
+        ref: 'Cliente',
+        required: true
+    },
+    fechacita: {
+        type: Date,
+        required: true
+    },
+    horacita: {  // Añadir hora específica
+        type: String,
+        required: true
+    },
+    duracionTotal: {  // Duración total en minutos
+        type: Number,
+        required: true
+    },
+    motivo: { 
+        type: String
+    },
+    fechacancelacion: {  
+        type: Date
+    },
+    montototal: {
+        type: Number,
+        required: true
+    },
+    estadocita: {
+        type: String,
+        enum: ['Pendiente', 'Confirmada', 'En Progreso', 'Completada', 'Cancelada'],
+        default: 'Pendiente'
+    },
+    servicios: [{
+        servicio: {
+            type: Schema.Types.ObjectId,
+            ref: 'Servicio'
+        },
+        nombreServicio: String,
+        precio: Number,
+        tiempo: Number
+    }]
+});
 
-module.exports = model("Cita", CitaSchema)
+module.exports = model('Cita', CitaSchema);
