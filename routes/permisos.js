83f55477f9bfcb4043b9e266e0be30db9217d1e2
IHomoSapienI <s-2221998@hotmail.com>
2025-06-02 07:47:40 -0500

    cambios en rutad epermisos

--- routes/permisos.js
+++ routes/permisos.js
@@ -12,16 +12,15 @@ const verificarPermisos = require("../middlewares/verificarPermisos")
 const verificarRolActivo = require("../middlewares/verificarRolActivo")
 
 // Aplicar middleware de autenticación y verificación de rol activo
-// router.use(validarJWT)
+router.use(validarJWT)
 router.use(verificarRolActivo)
 
 // Rutas para permisos con permisos específicos
-// router.get("/", verificarPermisos(["verPermisos"]), permisosGet)
-router.get("/", permisosGet)
+router.get("/", verificarPermisos(["verPermisos"]), permisosGet)
 router.post("/", verificarPermisos(["crearPermisos"]), permisosPost)
 router.put("/:id", verificarPermisos(["actualizarPermisos"]), permisosPut)
 router.delete("/:id", verificarPermisos(["eliminarPermisos"]), permisosDelete)
-router.get("/export-excel", permisosExportExcel)
+router.get("/export-excel", verificarPermisos(["verPermisos"]), permisosExportExcel)
 
 
 module.exports = router
